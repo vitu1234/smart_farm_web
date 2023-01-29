f034b59f5531f3fba958f8a13ecf98359a75046d
vitu1234 <mafeniblessed@gmail.com>
2023-01-29 16:24:44 +0900

    changed files to .php

---
+++ login.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.108.0">
+    <title>Smart Farm</title>
+
+    <!--    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/cover/">-->
+
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+
+        .b-example-divider {
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+        }
+
+        .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+        }
+
+        .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+        }
+
+        .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+        }
+
+        .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+</head>
+<body class="d-flex h-100 text-center  text-bg-secondary">
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto">
+        <div>
+            <h3 class="float-md-start mb-0">Smart Farm</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+                <a class="nav-link fw-bold py-1 px-0 " aria-current="page" href="indexSmart Farm">Home</a>
+                <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+                <a class="nav-link fw-bold py-1 px-0 active" href="#">Login</a>
+                <a class="nav-link fw-bold py-1 px-0" href="registerSmart Farm">Register</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="px-md-5 form-signin w-100 ">
+        <form class="px-md-5">
+            <!--      <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">-->
+            <h5 class="h5 mb-3 fw-normal"><strong>Please sign in</strong></h5>
+
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label class="text-dark" for="floatingInput">Email address</label>
+            </div>
+
+            <div class="form-floating mt-3 mb-1">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label class="text-dark" for="floatingPassword">Password</label>
+            </div>
+            <div class="form-floating mb-3">
+                <a class="link-light float-end" href="#">Forgot Password?</a>
+            </div>
+            <button class="w-100 btn btn-md btn-dark text-light mt-3" type="submit">Sign in</button>
+        </form>
+    </main>
+
+    <footer class="mt-auto text-white-50">
+        <p>
+            <script>document.write(new Date().getFullYear());</script> &copy; <span class="color-primary">DCN LAB</span>.
+            Designed by <a href="https://github.com/vitu1234" target="_blank"><span
+                class="text-light">Vitu Mafeni</span></a>
+        </p>
+    </footer>
+</div>
+
+
+</body>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.6.3.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(() => {
+        // alert("ffj")
+    })
+</script>
+</html>
